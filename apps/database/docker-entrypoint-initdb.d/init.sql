b7005c39c4bb80bdcc5d45560ed58c3b57c403cd
AltusBarry <AltusBarry@users.noreply.github.com>
2018-01-09 10:24:48 +0200

    feature/GE-684: Gave up, fully hardcode db creation

--- apps/database/docker-entrypoint-initdb.d/init.sql
+++ apps/database/docker-entrypoint-initdb.d/init.sql
@@ -1,14 +1,5 @@
-CREATE FUNCTION createdbs( names TEXT[] ) RETURNS void as $$
-
-DECLARE
-    name TEXT;
-
-BEGIN
-    FOREACH name IN ARRAY names LOOP
-        raise notice 'name: %', name;
-    END LOOP;
-END;
-
-$$ language plpgsql;
-
-SELECT createdbs( array['authservice','userdatastore','accesscontrol','wagtail_1','wagtail_2'] );
+CREATE DATABASE accesscontrol;
+CREATE DATABASE authservice;
+CREATE DATABASE userdatastore;
+CREATE DATABASE wagtail_1;
+CREATE DATABASE wagtail_2;
