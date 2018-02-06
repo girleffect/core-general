7672e7925038c1d4a1d9bd29deb67f0cae9658aa
cobusc <cobus.carstens@gmail.com>
2018-02-06 14:19:35 +0200

    Updated DB creation scripts

--- apps/database/docker-entrypoint-initdb.d/init.sql
+++ apps/database/docker-entrypoint-initdb.d/init.sql
@@ -1,5 +1,12 @@
-CREATE DATABASE access_control;
-CREATE DATABASE authentication_service;
-CREATE DATABASE user_data_store;
+CREATE USER access_control PASSWORD 'password';
+CREATE DATABASE access_control OWNER access_control;
+
+CREATE USER authentication_service PASSWORD 'password';
+CREATE DATABASE authentication_service OWNER authentication_service;
+
+CREATE USER user_data_store PASSWORD 'password';
+CREATE DATABASE user_data_store OWNER user_data_store;
+
 CREATE DATABASE wagtail_1;
+
 CREATE DATABASE wagtail_2;
