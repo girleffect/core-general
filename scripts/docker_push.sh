3763fc7decd86496b035852ede46b7500b3bdaff
cobusc <cobus.carstens@gmail.com>
2018-06-21 07:24:29 +0200

    Added Travis config and docker image build script

---
+++ scripts/docker_push.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+REPO=girleffect/robot-test-automation
+# Map "master" branch to "latest" tag. "develop" branch will have the "develop" tag.
+TAG=${TRAVIS_BRANCH/master/latest}
+# Tags may not contain slashes. Since git flow uses slashes as part of the branch name,
+# we replace all slashes with underscores, i.e. "release/1.0.0-alpha" becomes "release_1.0.0-alpha".
+TAG=${TAG//\//_}
+TAG=test
+
+docker build -t ${REPO}:${TAG} .
+docker login -u="${DOCKER_USERNAME}" -p="${DOCKER_PASSWORD}"
+docker push ${REPO}
