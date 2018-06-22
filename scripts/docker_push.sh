27e5db30438186b7de99c1a02c8e8e568be75984
cobusc <cobus.carstens@gmail.com>
2018-06-22 08:03:36 +0200

    Removed tag override

--- scripts/docker_push.sh
+++ scripts/docker_push.sh
@@ -6,7 +6,6 @@ TAG=${TRAVIS_BRANCH/master/latest}
 # Tags may not contain slashes. Since git flow uses slashes as part of the branch name,
 # we replace all slashes with underscores, i.e. "release/1.0.0-alpha" becomes "release_1.0.0-alpha".
 TAG=${TAG//\//_}
-TAG=test
 
 docker build -t ${REPO}:${TAG} .
 docker login -u="${DOCKER_USERNAME}" -p="${DOCKER_PASSWORD}"
