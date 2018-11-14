12b98b6252a73e2e6b8379a38c015926b6095d96
Aphiwe Moshesh <aphiwemoshesh@gmail.com>
2018-11-14 10:28:00 +0200

    set bash to verbose

--- scripts/SFTPImport.sh
+++ scripts/SFTPImport.sh
@@ -1,10 +1,8 @@
-#!/bin/bash
+#!/bin/bash -v
 
 # REMOTE_HOST, USERNAME, REMOTE_DIR, FILES
 
 RH_ERROR="Remote host is needed"
 RD_ERROR="Remote directory is needed"
 
-scp -rf ${FILES:- *} \
-"${USERNAME:- $(hostname -i)}@${REMOTE_HOST:? $RH_ERROR}:${REMOTE_DIR:? $RD_ERROR}" \
--P 22
+echo scp -rf ${FILES:- *} "${USERNAME:- $(hostname -i)}@${REMOTE_HOST:? $RH_ERROR}:${REMOTE_DIR:? $RD_ERROR}" -P ${PORT:- 22}
