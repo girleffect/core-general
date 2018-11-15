a49584bb6813095e86e9d75e0179c9594be9ca0f
Aphiwe Moshesh <aphiwemoshesh@gmail.com>
2018-11-15 10:54:23 +0200

    remove echo and -f flag

--- scripts/SFTPImport.sh
+++ scripts/SFTPImport.sh
@@ -5,6 +5,7 @@
 # usage `USERNAME=userftp REMOTE_HOST=127.0.0.1 PORT=22 FILES=*  REMOTE_DIR=/ ./scripts/SFTPImport.sh`
 
 RH_ERROR="Remote host is needed"
+RU_ERROR="Remote username is needed"
 RD_ERROR="Remote directory is needed"
 
-echo scp -rf ${FILES:- *} "${USERNAME:- $(hostname -i)}@${REMOTE_HOST:? $RH_ERROR}:${REMOTE_DIR:? $RD_ERROR}" -P ${PORT:- 22}
+scp -vr ${FILES:- *} "${USERNAME:? RU_ERROR}@${REMOTE_HOST:? $RH_ERROR}:${REMOTE_DIR:? $RD_ERROR}" -P ${PORT:- 22}
