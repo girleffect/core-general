d2b8d13e01b100af7ddbe9cddb8a0d2966b2b731
Aphiwe Moshesh <aphiwemoshesh@gmail.com>
2018-11-19 14:34:31 +0200

    update SFTP script

--- scripts/SFTPImport.sh
+++ scripts/SFTPImport.sh
@@ -2,20 +2,20 @@
 
 # REMOTE_HOST, USERNAME, REMOTE_DIR, FILES
 
-# usage `SFTPImport.sh -u test -h 127.0.0.1 -d "/remote/path" -f "*"`
+# usage `./SFTPImport.sh -u "bib" -h 197.210.150.250  -d /home/aphiwe/bar -f "GC_RAW_DATA/*.csv`
 
+D_ERROR="Directory is needed"
 RH_ERROR="Remote host is needed"
 RU_ERROR="Remote username is needed"
-RD_ERROR="Remote directory is needed"
 
 while getopts ":d:f:h:u:" option
 do
  case "${option}" in
- d) REMOTE_DIR=${OPTARG};;
+ d) DIR=${OPTARG};;
  f) FILES=${OPTARG};;
  h) REMOTE_HOST=${OPTARG};;
  u) USERNAME=${OPTARG};;
  esac
 done
 
-scp -vr ${FILES:- *} "${USERNAME:? $RU_ERROR}@${REMOTE_HOST:? $RH_ERROR}:${REMOTE_DIR:? $RD_ERROR}"
+scp -vr ${USERNAME:? $RU_ERROR}@${REMOTE_HOST:? $RH_ERROR}:${FILES:- *} ${DIR:? $D_ERROR}
