43fe967d343ba0724c9b41f1663e74e426d495a1
Aphiwe Moshesh <aphiwemoshesh@gmail.com>
2018-11-19 15:22:47 +0200

    Update script and use sftp instead of scp

--- scripts/SFTPImport.sh
+++ scripts/SFTPImport.sh
@@ -1,21 +1,23 @@
-#!/bin/sh -v
+#!/bin/sh -vx
 
 # REMOTE_HOST, USERNAME, REMOTE_DIR, FILES
 
-# usage `./SFTPImport.sh -u "bib" -h 197.210.150.250  -d /home/aphiwe/bar -f "GC_RAW_DATA/*.csv`
+# usage `./SFTPImport.sh -p <password> -u "bib" -h 197.210.150.250  -d /home/aphiwe/bar -f "GC_RAW_DATA/*.csv`
 
-D_ERROR="Directory is needed"
-RH_ERROR="Remote host is needed"
-RU_ERROR="Remote username is needed"
+D_ERROR="Directory is required"
+RH_ERROR="Remote host is required"
+U_ERROR="User password is required"
+RU_ERROR="Remote username is required"
 
-while getopts ":d:f:h:u:" option
+while getopts ":d:f:h:u:p:" option
 do
  case "${option}" in
  d) DIR=${OPTARG};;
  f) FILES=${OPTARG};;
  h) REMOTE_HOST=${OPTARG};;
  u) USERNAME=${OPTARG};;
+ p) PASSWORD=${OPTARG};;
  esac
 done
 
-scp -vr ${USERNAME:? $RU_ERROR}@${REMOTE_HOST:? $RH_ERROR}:${FILES:- *} ${DIR:? $D_ERROR}
+sshpass -p ${PASSWORD:? $U_ERROR} sftp -a ${USERNAME:? $RU_ERROR}@${REMOTE_HOST:? $RH_ERROR}:${FILES:- *} ${DIR:? $D_ERROR}
