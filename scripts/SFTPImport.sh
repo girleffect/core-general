f509e88639d1c408c35a6c22bcf0a899e62f1027
Aphiwe Moshesh <aphiwemoshesh@gmail.com>
2018-11-19 15:43:44 +0200

    update script and add comments on usage

--- scripts/SFTPImport.sh
+++ scripts/SFTPImport.sh
@@ -2,11 +2,17 @@
 
 # REMOTE_HOST, USERNAME, REMOTE_DIR, FILES
 
-# usage `./SFTPImport.sh -p <password> -u "bib" -h 197.210.150.250  -d /home/aphiwe/bar -f "GC_RAW_DATA/*.csv`
+# usage `./SFTPImport.sh -p <password> -u "bib" -h 197.210.150.250  -d /home/aphiwe/bar -f GC_RAW_DATA/*.csv`
+
+# -u Username
+# -p Password
+# -h ssh host machine
+# -d Is the directory to dump the contents of -f (files)
+# -f Files to Download from from remote machine
 
 D_ERROR="Directory is required"
 RH_ERROR="Remote host is required"
-U_ERROR="User password is required"
+P_ERROR="User password is required"
 RU_ERROR="Remote username is required"
 
 while getopts ":d:f:h:u:p:" option
@@ -20,4 +26,4 @@ do
  esac
 done
 
-sshpass -p ${PASSWORD:? $U_ERROR} sftp -a ${USERNAME:? $RU_ERROR}@${REMOTE_HOST:? $RH_ERROR}:${FILES:- *} ${DIR:? $D_ERROR}
+sshpass -p ${PASSWORD:? $P_ERROR} sftp -a ${USERNAME:? $RU_ERROR}@${REMOTE_HOST:? $RH_ERROR}:${FILES:- *} ${DIR:? $D_ERROR}
