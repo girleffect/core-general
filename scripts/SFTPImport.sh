eb39780789ee8b38adc381022c8e9a5c4a5ec46e
Aphiwe Moshesh <aphiwemoshesh@gmail.com>
2018-11-19 09:41:20 +0200

    remove port option

--- scripts/SFTPImport.sh
+++ scripts/SFTPImport.sh
@@ -2,21 +2,20 @@
 
 # REMOTE_HOST, USERNAME, REMOTE_DIR, FILES
 
-# usage `SFTPImport.sh -u test -h 127.0.0.1 -d "/remote/path" -p 22 -f "*"`
+# usage `SFTPImport.sh -u test -h 127.0.0.1 -d "/remote/path" -f "*"`
 
 RH_ERROR="Remote host is needed"
 RU_ERROR="Remote username is needed"
 RD_ERROR="Remote directory is needed"
 
-while getopts ":d:f:h:u:p" option
+while getopts ":d:f:h:u" option
 do
  case "${option}" in
  d) REMOTE_DIR=${OPTARG};;
  f) FILES=${OPTARG};;
  h) REMOTE_HOST=${OPTARG};;
  u) USERNAME=${OPTARG};;
- p) PORT=${OPTARG};;
  esac
 done
 
-scp -vr ${FILES:- *} "${USERNAME:? $RU_ERROR}@${REMOTE_HOST:? $RH_ERROR}:${REMOTE_DIR:? $RD_ERROR}" -P ${PORT:- 22}
+scp -vr ${FILES:- *} "${USERNAME:? $RU_ERROR}@${REMOTE_HOST:? $RH_ERROR}:${REMOTE_DIR:? $RD_ERROR}"
