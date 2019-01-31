ec5a2f8c1a707a1ed6f7536b1eab1c3c8d24773e
cobusc <cobus.carstens@gmail.com>
2019-01-31 14:35:37 +0200

    Added prod endpoint to script

--- utils/query_elasticsearch.py
+++ utils/query_elasticsearch.py
@@ -6,7 +6,7 @@
 
 # Usage:
 #
-# python query_elasticsearch.py 'RESOURCE_CRUD' | python -m json.tool | less
+# python query_elasticsearch.py qa 'RESOURCE_CRUD' | python -m json.tool | less
 #
 # Make sure your ElasticSearch credentials are stored in `~/.aws/config` in a
 # profile called "es-cli" (or modify this script) to support it as an arg.
@@ -18,19 +18,29 @@ import boto3
 import requests
 import sys
 
-host = 'https://search-ge-event-log-qeyhk65xsespxycxrxbw3ievfq.eu-west-1.es.amazonaws.com'
+env_to_host_map = {
+    "qa": 'https://search-ge-event-log-qeyhk65xsespxycxrxbw3ievfq.eu-west-1.es.amazonaws.com',
+    "prod": 'https://search-ge-event-log-goljhod5hxs72upk3oba2hwq2y.eu-west-1.es.amazonaws.com'
+}
 path = '/'
 region = 'eu-west-1'
 
 service = 'es'
-credentials = boto3.Session(profile_name="es-cli").get_credentials()
+
+env_to_profile_name_map = {
+    "prod": "prod-es-cli",
+    "qa": "qa-es-cli"
+}
+
+environment = sys.argv[1]
+credentials = boto3.Session(profile_name=env_to_profile_name_map[environment]).get_credentials()
 awsauth = AWS4Auth(credentials.access_key, credentials.secret_key, region, service)
 
 params = {
-    "q": sys.argv[1],
+    "q": sys.argv[2],
     "sort": "timestamp:desc"
 }
-url = host + path + "_search?" + urlencode(params)
+url = env_to_host_map[environment] + path + "_search?" + urlencode(params)
 
 r = requests.get(url, auth=awsauth)
 
